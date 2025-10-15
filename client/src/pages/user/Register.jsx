b4d7064d926ff1bb6e82c763330daa0adedab7cc
Daniel-Yasman <daniel.yasman1998@gmail.com>
2025-10-15 17:15:06 +0300

    fix(Register): add Link import

--- client/src/pages/user/Register.jsx
+++ client/src/pages/user/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 function Register() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
