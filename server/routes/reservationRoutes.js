7a3ffc0df567327840cc11bd4f2963a268208aa5
Daniel <daniel.yasman1998@gmail.com>
2025-11-02 12:16:53 +0200

    feat(reservationRoutes): add user fetcher into routes

--- server/routes/reservationRoutes.js
+++ server/routes/reservationRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const {
   createReservation,
+  listUserReservations,
   userDeleteReservation,
   listAllReservations,
 } = require("../controllers/reservationController");
 
 router.post("/:userId", createReservation);
+router.get("/:userId", listUserReservations);
 router.delete("/:userId/:reservationId", userDeleteReservation);
 router.get("/", listAllReservations);
-
 module.exports = router;
